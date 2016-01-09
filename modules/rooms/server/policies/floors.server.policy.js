3b11fa15dae04fd2e692eb8b1413bd18126194d1
Marek Grzybek <feimosi@onet.pl>
2016-01-09 19:04:37 +0100

    Remove user check in policy rules
    It makes no sense as we don't store the creator in these objects

--- modules/rooms/server/policies/floors.server.policy.js
+++ modules/rooms/server/policies/floors.server.policy.js
@@ -38,11 +38,6 @@ exports.invokeRolesPolicies = function () {
 exports.isAllowed = function (req, res, next) {
     var roles = (req.user) ? req.user.roles : ['guest'];
 
-    // If an floor is being processed and the current user created it then allow any manipulation
-    if (req.floor && req.user && req.floor.user.id === req.user.id) {
-        return next();
-    }
-
     // Check for user roles
     acl.areAnyRolesAllowed(roles, req.route.path, req.method.toLowerCase(), function (err, isAllowed) {
         if (err) {
