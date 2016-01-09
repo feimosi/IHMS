c7f54b912ae9f4b17c6e1e2ddfe44d0ea6f0ba81
Marek Grzybek <feimosi@onet.pl>
2016-01-09 20:57:08 +0100

    Update header title and menu

--- modules/rooms/client/config/rooms.client.config.js
+++ modules/rooms/client/config/rooms.client.config.js
@@ -2,20 +2,8 @@
 
 angular.module('rooms').run(function (Menus) {
     Menus.addMenuItem('topbar', {
-        title: 'Rooms',
-        state: 'rooms',
-        type: 'dropdown',
+        title: 'Rooms management',
+        state: 'rooms.list',
         roles: ['*']
-    });
-
-    Menus.addSubMenuItem('topbar', 'rooms', {
-        title: 'List Rooms',
-        state: 'rooms.list'
-    });
-
-    Menus.addSubMenuItem('topbar', 'rooms', {
-        title: 'Create Room',
-        state: 'rooms.create',
-        roles: ['user']
     });
 });
