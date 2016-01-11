28bb84c78aef0e2535c536c5bfa700d193d8c9de
Marek Grzybek <feimosi@onet.pl>
2016-01-11 11:34:33 +0100

    Change parameterized variable name

--- modules/room-features/client/services/room-feature-types.client.service.js
+++ modules/room-features/client/services/room-feature-types.client.service.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('room-features').factory('RoomFeatureTypes', function ($resource) {
-    return $resource('api/room-feature-types/:roomId', {
-        roomId: '@_id'
+    return $resource('api/room-feature-types/:id', {
+        id: '@_id'
     }, {
         update: {
             method: 'PUT'
