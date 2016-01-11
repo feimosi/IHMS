3c491cc227daddf12c28b15b5108382942e02b25
Marek Grzybek <feimosi@onet.pl>
2016-01-11 13:24:08 +0100

    Move rooms filter to a separate file

--- modules/rooms/client/controllers/rooms.client.controller.js
+++ modules/rooms/client/controllers/rooms.client.controller.js
@@ -17,40 +17,4 @@ angular.module('rooms').controller('RoomsManagementController', function ($scope
     };
 
     vm.updateRoomsList();
-}).filter('roomsFilter', function () {
-    function byRoomNumber(rooms, roomNumberFilter) {
-        var filtered = [];
-        var roomNumbers = roomNumberFilter.split(/[,|\s]\s*/);
-        roomNumbers.forEach(function (roomNumber) {
-            filtered = filtered.concat(rooms.filter(function (room) {
-                return room.number === +roomNumber;
-            }));
-        });
-        return filtered;
-    }
-
-    function byFloorNumber(rooms, floorNumberFilter) {
-        var filtered = [];
-        var floorNumbers = floorNumberFilter.split(/[,|\s]\s*/);
-        floorNumbers.forEach(function (floorNumber) {
-            filtered = filtered.concat(rooms.filter(function (room) {
-                return room.floor.number === +floorNumber;
-            }));
-        });
-        return filtered;
-    }
-
-    return function (rooms, filters) {
-        var isFiltered = false;
-        var filtered = [];
-        if (filters.roomNumber) {
-            isFiltered = true;
-            filtered = byRoomNumber(rooms, filters.roomNumber);
-        }
-        if (filters.floorNumber) {
-            isFiltered = true;
-            filtered = byFloorNumber(filtered.length ? filtered : rooms, filters.floorNumber);
-        }
-        return isFiltered ? filtered : rooms;
-    };
 });
