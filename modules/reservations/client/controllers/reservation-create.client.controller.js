a06b92a4cafb6edcac45f9047730cfd6a04a43dc
Marek Grzybek <feimosi@onet.pl>
2016-01-24 23:02:21 +0100

    Implement Reservation Create view

---
+++ modules/reservations/client/controllers/reservation-create.client.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('reservations').controller('ReservationCreateController', function ($scope, $state, $stateParams) {
+    var vm = this;
+    console.log($stateParams);
+    vm.personalData = {};
+
+    vm.next = function () {
+        $state.go('reservations.list.confirm', {
+            room: $stateParams.room,
+            personalData: vm.personalData
+        });
+        $scope.$close();
+    };
+
+    vm.dismiss = function () {
+        $scope.$dismiss();
+    };
+});
