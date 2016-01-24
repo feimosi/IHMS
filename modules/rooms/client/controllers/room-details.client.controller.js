776fdf498775bb9b3da119d380bdb3eee6231126
Marek Grzybek <feimosi@onet.pl>
2016-01-25 00:01:53 +0100

    Display room description in a separate block

--- modules/rooms/client/controllers/room-details.client.controller.js
+++ modules/rooms/client/controllers/room-details.client.controller.js
@@ -4,8 +4,16 @@ angular.module('rooms').controller('RoomDetailsController', function ($scope, $s
                                                                       Rooms) {
     var vm = this;
     vm.room = Rooms.get({ roomId: $stateParams.roomId }, function () {
-
+        vm.room.features.forEach(function (feature) {
+            if (feature.type.name === 'Description') {
+                vm.room.description = feature.value;
+            }
+        });
     });
+
+    vm.exceptDetails = function (feature, index) {
+        return feature.type.name !== 'Description';
+    }
 
     vm.close = function () {
         $scope.$close();
