ea945aa012e2991c63de92f0c44ec054b061e9a2
Marek Grzybek <feimosi@onet.pl>
2016-01-24 23:01:25 +0100

    Add routes to the booking process

--- modules/reservations/client/config/reservations.client.routes.js
+++ modules/reservations/client/config/reservations.client.routes.js
@@ -10,5 +10,42 @@ angular.module('reservations').config(function ($stateProvider) {
         .state('reservations.list', {
             url: '',
             templateUrl: 'modules/reservations/client/views/reservations-list.client.view.html'
+        })
+        .state('reservations.list.create', {
+            url: '/book',
+            data: {
+                roles: ['user', 'admin']
+            },
+            params: {
+                room: null
+            },
+            onEnter: function ($document, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'modules/reservations/client/views/reservation-create.client.view.html'
+                }).result.then(function () {
+
+                }).catch(function () {
+                    $state.go('reservations.list');
+                });
+            }
+        })
+        .state('reservations.list.confirm', {
+            url: '/confirm',
+            data: {
+                roles: ['user', 'admin']
+            },
+            params: {
+                room: null,
+                personalData: null
+            },
+            onEnter: function ($document, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'modules/reservations/client/views/reservation-confirm.client.view.html'
+                }).result.then(function () {
+                    $state.go('reservations.list.view');
+                }).catch(function () {
+                    $state.go('reservations.list');
+                });
+            }
         });
 });
