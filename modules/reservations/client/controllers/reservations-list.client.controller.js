797e16cd8325a178ee6a007ad34cb506019b6127
Marek Grzybek <feimosi@onet.pl>
2016-01-27 21:44:47 +0100

    Add datepicker for reservation start and end

--- modules/reservations/client/controllers/reservations-list.client.controller.js
+++ modules/reservations/client/controllers/reservations-list.client.controller.js
@@ -1,9 +1,20 @@
 'use strict';
 
-angular.module('reservations').controller('ReservationsListController', function (Rooms, RoomFeatureTypes) {
+angular.module('reservations').controller('ReservationsListController', function ($scope, Rooms, RoomFeatureTypes) {
     var vm = this;
     vm.currency = 'EUR';
     vm.filters = {};
+    vm.datepickerOptions = {
+        startingDay: 1,
+        showWeeks: false,
+        showButtonBar: false
+    };
+    vm.datepickerStartOpened = false;
+    vm.datepickerEndOpened = false;
+    vm.dateStart = Date.now();
+    vm.dateEnd = null;
+    vm.minStartDate = Date.now();
+    vm.minEndDate = null;
     vm.rooms = Rooms.query(function () {
         // Convert features array to a map
         vm.rooms.forEach(function (room) {
@@ -19,5 +30,23 @@ angular.module('reservations').controller('ReservationsListController', function
             obj[feature.name] = feature;
             return obj;
         }, {});
+    });
+
+    vm.openDatepickerStart = function openDatepickerStart() {
+        vm.datepickerStartOpened = true;
+    };
+
+    vm.openDatepickerEnd = function openDatepickerEnd() {
+        vm.datepickerEndOpened = true;
+    };
+
+    $scope.$watch(function () {
+        return vm.dateStart;
+    }, function () {
+        if (vm.dateStart !== null) {
+            var dateStart = new Date(vm.dateStart);
+            dateStart.setDate(dateStart.getDate() + 1);
+            vm.minEndDate = dateStart;
+        }
     });
 });
