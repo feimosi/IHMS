4150ad34f65488b017eaed4afb09f8f38ebf5f06
Marek Grzybek <feimosi@onet.pl>
2016-01-27 23:12:08 +0100

    Send new reservation details to the server

--- modules/reservations/client/controllers/reservation-confirm.client.controller.js
+++ modules/reservations/client/controllers/reservation-confirm.client.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('reservations').controller('ReservationConfirmController', function ($scope, $state, $stateParams) {
+angular.module('reservations').controller('ReservationConfirmController', function ($scope, $state, $stateParams,
+                                                                                    Reservations, Authentication,
+                                                                                    Notification) {
     var vm = this;
     vm.currency = 'EUR';
     vm.room = $stateParams.reservation.room;
@@ -9,11 +11,27 @@ angular.module('reservations').controller('ReservationConfirmController', functi
     vm.personalData = $stateParams.personalData;
 
     vm.confirm = function () {
-        $state.go('reservations.list.summary', {
-            room: vm.room,
-            personalData: vm.personalData
+        var reservation = new Reservations({
+            room: vm.room._id,
+            user: Authentication.user._id,
+            startDate: vm.startDate,
+            endDate: vm.endDate
         });
-        $scope.$close();
+
+        reservation.$save(function (response) {
+            $state.go('reservations.list.summary', {
+                reservation: $stateParams.reservation,
+                personalData: $stateParams.personalData
+            });
+            Notification.success('Reservation process has completed successfully');
+            $scope.$close(response);
+        }, function (errorResponse) {
+            if (errorResponse.data.message.includes('reservation needs an end date')) {
+                Notification.warning('You can\'t make a reservation without end date specified');
+            } else {
+                vm.error = errorResponse.data.message;
+            }
+        });
     };
 
     vm.dismiss = function () {
